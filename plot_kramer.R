b61d36d037c934ccdcd4f1681f686e88b758e7f4
irenecrsn <icordoba@fi.upm.es>
2019-08-30 14:01:16 +0200

    Improve plot_kramer

--- plot_kramer.R
+++ plot_kramer.R
@@ -2,7 +2,8 @@ library("reshape2")
 library("RColorBrewer")
 library("ggplot2")
 
-plot_kramer <- function(algorithm, dir_name, fname, plot_title, stat = "ppv") {
+plot_kramer <- function(algorithm, dir_name, fname, plot_title, subtitle,
+                        stat = "ppv") {
 
 	methods <- c("adalasso", "lasso", "pls", "shrink", "ridge")
 	ylab <- c("ppv" = "True discovery rate", "tpr" = "True positive rate")
@@ -24,24 +25,39 @@ plot_kramer <- function(algorithm, dir_name, fname, plot_title, stat = "ppv") {
 	pl <- ggplot(df, aes(x = N, y = value, group = method, color = method)) +
 		geom_line() +
 		geom_point() +
-		theme(text = element_text(size = 20), legend.position = "bottom") +
+		theme(text = element_text(size = 20), legend.position = "bottom",
+		      legend.title = element_blank()) +
 		scale_color_manual(values = colpal) +
 		xlab("Sample size") +
 		ylab(ylab[stat]) +
 		ylim(0, 1) +
-		ggtitle(plot_title)
+		ggtitle(plot_title, subtitle = subtitle)
 
 	dir.create(path = "plot_kramer/", showWarnings = FALSE)
 	ggsave(filename = paste0("plot_kramer/", stat, "_", algorithm, ".pdf"))
 }
 
-plot_kramer(algorithm = "diagdom", dir_name = "res_kramer_0.25",
-	plot_title = "Diagonal dominance method", stat = "ppv")
-plot_kramer(algorithm = "diagdom", dir_name = "res_kramer_0.25",
-	plot_title = "Diagonal dominance method", stat = "tpr")
+# Experiment scenarios
+densities <- c("0.25", "0.05")
+stats <- c("ppv", "tpr")
+methods <- c("diagdom", "port", "port_chol")
+titles <- c("diagdom" = "Diagonal dominance",
+			"port" = "Partial orthogonalization",
+			"port_chol" = "Uniform + partial orthogonalization")
 
-plot_kramer(algorithm = "port", dir_name = "res_kramer_port_0.25",
-	plot_title = "Partial orthogonalization method", stat = "ppv")
-plot_kramer(algorithm = "port", dir_name = "res_kramer_port_0.25",
-	plot_title = "Partial orthogonalization method", stat = "tpr")
+for (d in densities) {
+  for (method in methods) {
+    for (s in stats) {
+	    dir_name <- paste0("res_kramer_", method, "_", d)
+		# Only plot if the experiment has been run
+		if (file.exists(dir_name)) {
+			plot_kramer(algorithm = paste0(method, "_", d),
+				dir_name = dir_name,
+				plot_title = titles[method],
+				subtitle = paste0("d = ", d),
+				stat = s)
+		}
+    }
+  }
+}
 
