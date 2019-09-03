8f469d178f1c9747a6fa6dc43b5d026f8b357d49
irenecrsn <icordoba@fi.upm.es>
2019-09-03 12:26:57 +0200

    Parallelize kramer experiment. Improve plot script

--- plot_kramer.R
+++ plot_kramer.R
@@ -2,62 +2,63 @@ library("reshape2")
 library("RColorBrewer")
 library("ggplot2")
 
-plot_kramer <- function(algorithm, dir_name, fname, plot_title, subtitle,
-                        stat = "ppv") {
-
-	methods <- c("adalasso", "lasso", "pls", "shrink", "ridge")
-	ylab <- c("ppv" = "True discovery rate", "tpr" = "True positive rate")
-	N <- seq(25, 200, 25)
-	power <- array(dim = c(length(methods), length(N)),
-							 dimnames = list(method = methods, N = N))
-
-	for (method in methods) {
-		power.raw <- readRDS(paste0(dir_name, "/", stat, ".", method, ".rds"))
-		power[method, ] <- apply(X = power.raw, MARGIN = 2, mean)
-	}
-
-	palette <- colorRampPalette(colors = c("black", "red"))
-	colpal <- brewer.pal(name = "Set1", n = length(N))
-
-	df <- melt(power)
-	df$method <- as.factor(df$method)
-
-	pl <- ggplot(df, aes(x = N, y = value, group = method, color = method)) +
-		geom_line() +
-		geom_point() +
-		theme(text = element_text(size = 20), legend.position = "bottom",
-		      legend.title = element_blank()) +
-		scale_color_manual(values = colpal) +
-		xlab("Sample size") +
-		ylab(ylab[stat]) +
-		ylim(0, 1) +
-		ggtitle(plot_title, subtitle = subtitle)
-
-	dir.create(path = "plot_kramer/", showWarnings = FALSE)
-	ggsave(filename = paste0("plot_kramer/", stat, "_", algorithm, ".pdf"))
-}
-
 # Experiment scenarios
-densities <- c("0.25", "0.05")
-stats <- c("ppv", "tpr", "MSE", "selected")
-methods <- c("diagdom", "port", "port_chol")
-titles <- c("diagdom" = "Diagonal dominance",
-			"port" = "Partial orthogonalization",
-			"port_chol" = "Uniform + partial orthogonalization")
+density <- c("0.25", "0.05")
+title <- c(
+  "diagdom" = "Diagonal dominance",
+  "port" = "Partial orthogonalization",
+  "port_chol" = "Uniform + partial orthogonalization"
+)
+method <- names(title)
+ylab <- c(
+  "ppv" = "True discovery rate",
+  "tpr" = "True positive rate",
+  "MSE" = "Mean squared error",
+  "selected" = "Significant edges"
+)
+stat <- names(ylab)
+algo <- c("adalasso", "lasso", "pls", "shrink", "ridge")
+N <- seq(25, 200, 25)
+estimate <- array(
+  dim = c(length(algo), length(N)),
+  dimnames = list(algo = algo, N = N)
+)
+dir_plot <- "plot_kramer"
 
-for (d in densities) {
-  for (method in methods) {
-    for (s in stats) {
-	    dir_name <- paste0("res_kramer_", method, "_", d)
-		# Only plot if the experiment has been run
-		if (file.exists(dir_name)) {
-			plot_kramer(algorithm = paste0(method, "_", d),
-				dir_name = dir_name,
-				plot_title = titles[method],
-				subtitle = paste0("d = ", d),
-				stat = s)
-		}
+dir.create(path = dir_plot, showWarnings = FALSE)
+
+for (d in density) {
+  for (m in method) {
+    for (s in stat) {
+      dir_exp <- paste0("res_kramer_", m, "_", d)
+      # Only plot if the experiment has been run
+      if (file.exists(dir_exp)) {
+        for (i in 1:length(algo)) {
+          estimate.raw <- readRDS(paste0(dir_exp, "/", s, ".", algo[i], ".rds"))
+          estimate[algo[i], ] <- apply(X = estimate.raw, MARGIN = 2, mean)
+        }
+
+        palette <- colorRampPalette(colors = c("black", "red"))
+        colpal <- brewer.pal(name = "Set1", n = length(N))
+
+        df <- melt(estimate)
+        df$algo <- as.factor(df$algo)
+
+        pl <- ggplot(df, aes(x = N, y = value, group = algo, color = algo)) +
+          geom_line() +
+          geom_point() +
+          theme(
+            text = element_text(size = 20), legend.position = "bottom",
+            legend.title = element_blank()
+          ) +
+          scale_color_manual(values = colpal) +
+          xlab("Sample size") +
+          ylab(ylab[s]) +
+          # ylim(0, 1) +
+          ggtitle(title[m], subtitle = paste0("d = ", d))
+
+        ggsave(filename = paste0(dir_plot, "/", s, "_", m, "_", d, ".pdf"))
+      }
     }
   }
 }
-
