516e41a0bbd9540436fd68c433736f0104ed0f2e
irenecrsn <icordoba@fi.upm.es>
2020-01-15 15:27:56 +0100

    Fix experiment

--- experiment.R
+++ experiment.R
@@ -3,10 +3,9 @@ p <- c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 125, 150, 200, 250, 300, 400, 50
 d <- c(0.0025, 0.005, 0.025, 0.05, 0.25, 0.5)
 N <- 10
 
-dname <- "sim"
-ggmexp::execute(p = p, d = d, r = r, dir = dname, ename = "diagdom",
+ggmexp::execute(p = p, d = d, r = r, ename = "diagdom",
 								emethod = gmat::diagdom, N = N)
-ggmexp::execute(p = p, d = d, r = r, dir = dname, ename = "port", 
+ggmexp::execute(p = p, d = d, r = r, ename = "port", 
 								emethod = gmat::port, N = N)
 
 p <- seq(from = 10, to = 200, by = 10)
@@ -21,8 +20,7 @@ get_time <- function(method, ...) {
 	return(difftime(t_end, t_begin, unit = "secs"))
 }
 
-dname <- "time"
-ggmexp::execute(p = p, d = d, r = r, dir = dname, ename = "t_diagdom",
+ggmexp::execute(p = p, d = d, r = r, ename = "t_diagdom",
 								emethod = get_time, method = diagdom, N = N)
-ggmexp::execute(p = p, d = d, r = r, dir = dname, ename = "t_port", 
+ggmexp::execute(p = p, d = d, r = r, ename = "t_port", 
 								emethod = get_time, method = port, N = N)
