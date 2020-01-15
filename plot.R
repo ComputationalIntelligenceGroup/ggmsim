ae75e1ac2c70483255e1b6f53ad2414a02215933
irenecrsn <icordoba@fi.upm.es>
2020-01-15 15:52:53 +0100

    Use package ggmexp for some plots

--- plot.R
+++ plot.R
@@ -4,51 +4,54 @@ N <- 10
 p <- c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 125, 150, 200, 250, 300, 400, 500, 750, 1000)
 d <- c(0.0025, 0.005, 0.025, 0.05, 0.25, 0.5)
 r <- 10
-dir_name <- paste0("res_r", 1:r)
+
+dname <- "plot/"
+max_abs_offdiag <- function(mat) {
+	return(max(0, abs((mat / diag(mat))[upper.tri(mat) & mat != 0])))
+}
 
 ## Plot all scenarios with both methods (average)
-plot_map_reduce(
-  p = p, d = d, N = N, map = ext_abs_offdiag, reduce = mean, dir_name = dir_name,
-  method = "port", fname = "port_avg_max_abs_offdiag.pdf", fext = max,
-  show_sd = FALSE, plot_title = "Partial orthogonalization method",
-  plot_ylab = "Average R"
-)
-plot_map_reduce(
-  p = p, d = d, N = N, map = ext_abs_offdiag, reduce = mean, dir_name = dir_name,
-  method = "diagdom", fname = "diagdom_avg_max_abs_offdiag.pdf",
-  fext = max, show_sd = FALSE, plot_title = "Diagonal dominance method",
-  plot_ylab = "Average R"
-)
+pl <- ggmexp::plot_map_reduce(p = p, d = d, N = N, map = max_abs_offdiag, 
+															reduce = mean, ename = "port",
+															plot_title = "Partial orthogonalization",
+															plot_ylab = "Average R", show_sd = FALSE)
+ggsave(filename = "port_avg_max_abs_offdiag.pdf", plot = pl, device = "pdf", 
+			 path = dname)
+
+pl <- ggmexp::plot_map_reduce(p = p, d = d, N = N, map = max_abs_offdiag, 
+															reduce = mean, ename = "diagdom", 
+															plot_title = "Diagonal dominance", 
+															plot_ylab = "Average R", show_sd = FALSE)
+ggsave(filename = "diagdom_avg_max_abs_offdiag.pdf", plot = pl, device = "pdf", 
+			 path = dname)
 
 ## Plot a comparison of sparsest vs densest scenario, with standard deviation
 plot_map_reduce_cmp(
-  p = p, d = d[1], N = N, map = ext_abs_offdiag, reduce = mean, dir_name = dir_name,
+  p = p, d = d[1], N = N, map = max_abs_offdiag, reduce = mean, dir_name = dir_name,
   plot_title = paste0("d = ", d[1]), show_sd = TRUE, plot_ylab = "Average R",
   fname = paste0("cmp_avg_max_abs_offdiag_", d[1], ".pdf")
 )
 
 plot_map_reduce_cmp(
-  p = p, d = d[length(d)], N = N, map = ext_abs_offdiag, reduce = mean, dir_name = dir_name,
+  p = p, d = d[length(d)], N = N, map = max_abs_offdiag, reduce = mean, dir_name = dir_name,
   plot_title = paste0("d = ", d[length(d)]), show_sd = TRUE, plot_ylab = "Average R",
   fname = paste0("cmp_avg_max_abs_offdiag_", d[length(d)], ".pdf")
 )
 
 ## Plot condition numbers of port matrices
 p <- c(10, 20, 30, 40, 50, 60, 70, 80, 90, 100)
-plot_map_reduce(
-  p = p, d = d, N = N, map = kappa, reduce = median, dir_name = dir_name,
-  method = "port", fname = "port_median_kappa.pdf",
-  plot_title = "Different structure densities",
-  plot_ylab = "Median of K", exact = TRUE
-)
+pl <- ggmexp::plot_map_reduce(p = p, d = d, N = N, map = kappa, reduce = median, 
+												ename = "port", plot_title = "Different structure densities",
+												plot_ylab = "Median of K", exact = TRUE)
+ggsave(filename = "port_median_kappa.pdf", plot = pl, device = "pdf", 
+			 path = dname)
 
 d <- c(0.0025, 0.005, 0.025, 0.05)
-plot_map_reduce(
-  p = p, d = d, N = N, map = kappa, reduce = median, dir_name = dir_name,
-  method = "port", fname = "port_median_kappa_trimmed.pdf",
-  plot_title = "Sparse graph structures",
-  plot_ylab = "Median of K", exact = TRUE
-)
+pl <- ggmexp::plot_map_reduce(p = p, d = d, N = N, map = kappa, reduce = median, 
+												ename = "port", plot_title = "Sparse graph structures",
+												plot_ylab = "Median of K", exact = TRUE)
+ggsave(filename = "port_median_kappa_trimmed.pdf", plot = pl, device = "pdf", 
+			 path = dname)
 
 ## Plot time comparison results
 p <- seq(from = 10, to = 200, by = 10)
--- plot_utils.R
+++ plot_utils.R
@@ -1,65 +1,6 @@
 library("ggplot2")
 library("RColorBrewer")
 
-plot_map_reduce <- function(p, d, N, map = function(x) {
-                              return(x)
-                            },
-                            reduce, method, fname, show_sd = FALSE,
-                            dir_name = "res", plot_title = "", plot_ylab = "", ...) {
-  d_len <- length(d)
-  dir_len <- length(dir_name)
-  exp_name <- outer(p, d, paste, sep = "_")
-  exp_fname <- matrix(paste0(exp_name, ".rds"), ncol = d_len)
-
-  res <- matrix(
-    nrow = length(p), ncol = d_len,
-    dimnames = list(p = p, d = d)
-  )
-  res_sd <- matrix(
-    nrow = length(p), ncol = d_len,
-    dimnames = list(p = p, d = d)
-  )
-
-
-  for (i in 1:length(p)) {
-    sample <- array(dim = c(p[i], p[i], N * dir_len))
-    for (j in 1:d_len) {
-      for (k in 1:dir_len) {
-        sample[, , ((k - 1) * N + 1):(k * N)] <-
-          readRDS(file = paste0(dir_name[k], "/", method, "_", exp_fname[i, j]))
-      }
-      mapd_mat <- apply(sample, MARGIN = 3, map, ...)
-      res[i, j] <- reduce(mapd_mat)
-      res_sd[i, j] <- sd(mapd_mat)
-    }
-  }
-
-  wd <- getwd()
-  dir.create(paste0(wd, "/plot_", dir_len), showWarnings = FALSE)
-
-  palette <- colorRampPalette(colors = c("black", "red"))
-  colors <- palette(d_len)
-
-  df <- reshape2::melt(res)
-  df$d <- as.factor(df$d)
-  df$sd <- reshape2::melt(res_sd)$value
-
-  pl <- ggplot(df, aes(x = p, y = value, group = d, color = d)) +
-    geom_line() +
-    geom_point() +
-    theme(text = element_text(size = 20), legend.position = "bottom") +
-    scale_color_manual(values = colors) +
-    xlab("Number of nodes") +
-    ylab(plot_ylab) +
-    ggtitle(plot_title)
-
-  if (show_sd == TRUE) {
-    pl <- pl + geom_ribbon(aes(ymin = value - sd, ymax = value + sd))
-  }
-
-  ggsave(filename = fname, plot = pl, device = "pdf", path = paste0("plot_", dir_len, "/"))
-}
-
 plot_map_reduce_cmp <- function(p, d, N, map = function(x) {
                                   return(x)
                                 },
@@ -72,7 +13,6 @@ plot_map_reduce_cmp <- function(p, d, N, map = function(x) {
     ncol = 2,
     dimnames = list(p = p, method = methods), byrow = TRUE
   )
-
 
   res <- matrix(
     nrow = length(p), ncol = 2,
@@ -124,14 +64,6 @@ plot_map_reduce_cmp <- function(p, d, N, map = function(x) {
 
   ggsave(filename = fname, plot = pl, device = "pdf", path = paste0("plot_", dir_len, "/"))
 }
-
-ext_abs_offdiag <- function(mat, fext = max, inverse = FALSE) {
-  if (inverse == TRUE) {
-    mat <- Matrix::solve(drop0(mat))
-  }
-  return(fext(0, abs((mat / diag(mat))[upper.tri(mat) & mat != 0])))
-}
-
 
 plot_time <- function(p, d, method, fname, dir_name = "res", plot_title = "", ...) {
   d_len <- length(d)
