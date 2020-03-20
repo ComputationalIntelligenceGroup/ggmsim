d27d9c1233c4a1721f6e3b15666dc6831f8e5520
irenecrsn <icordoba@fi.upm.es>
2020-03-20 18:43:10 +0100

    update kramer plot file

--- plot_kramer.R
+++ plot_kramer.R
@@ -2,25 +2,14 @@ library("reshape2")
 library("ggplot2")
 
 # Experiment scenarios
-density <- c("0.25", "0.2", "0.15", "0.1", "0.05")
-title <- c(
-  "diagdom" = "Diagonal dominance",
-  "port" = "Partial orthogonalization",
-  "port_chol" = "Uniform + partial orthogonalization"
-)
-method <- names(title)
-ylab <- c(
-  "ppv" = "True discovery rate",
-  "tpr" = "True positive rate",
-  "MSE" = "Mean squared error",
-  "selected" = "Significant edges"
-)
-stat <- names(ylab)
+density <- c("0.25", "0.05")
+method <- c("diagdom", "port_chol")
+stat <- c("ppv", "tpr")
 algo <- c("adalasso", "lasso", "pls", "shrink", "ridge")
 N <- seq(25, 200, 25)
 estimate <- array(
-  dim = c(length(algo), length(N)),
-  dimnames = list(algo = algo, N = N)
+  dim = c(length(algo), length(N), length(method), length(stat)),
+  dimnames = list(algo = algo, N = N, method = method, stat = stat)
 )
 dir_plot <- "plot_kramer"
 
@@ -34,35 +23,27 @@ for (d in density) {
       if (file.exists(dir_exp)) {
         for (i in 1:length(algo)) {
           estimate.raw <- readRDS(paste0(dir_exp, "/", s, ".", algo[i], ".rds"))
-          estimate[algo[i], ] <- apply(X = estimate.raw, MARGIN = 2, mean)
+          estimate[algo[i], , m, s] <- apply(X = estimate.raw, MARGIN = 2, mean)
         }
-
-        palette <- grDevices::colorRampPalette(colors = c("black", "red"))
-        colpal <- palette(length(N))
-
-        df <- melt(estimate)
-        df$algo <- as.factor(df$algo)
-
-        ggplot(df, aes(x = N, y = value, group = algo, color = algo)) +
-          geom_line() +
-          geom_point() +
-          theme(
-            text = element_text(size = 20), legend.position = "bottom",
-            legend.title = element_blank()
-          ) +
-          scale_color_manual(values = colpal) +
-          xlab("Sample size") +
-          ylab(ylab[s]) +
-          ylim(0, 1) + # This should be different for selected and MSE
-          ggtitle(title[m], subtitle = paste0("d = ", d)) +
-          ggsave(
-            filename = paste0(
-              dir_plot, "/", s, "_", m, "_",
-              sub("\\.", "", d), ".pdf"
-            ),
-            width = 7, height = 4
-          )
-      }
-    }
+	  }
+	}
   }
-}
+  df <- melt(estimate)
+  df$algo <- as.factor(df$algo)
+  df$method <- as.factor(df$method)
+  df$stat <- as.factor(df$stat)
+
+  ggplot(df, aes(x = N, y = value, group = algo, color = algo)) +
+  		  facet_grid(rows = vars(stat), cols = vars(method), scales = "free",
+		  	labeller = labeller(stat = toupper)) +
+          geom_line() +
+          theme_bw() +
+		  theme(legend.position = "bottom", legend.title = element_blank()) +
+          xlab("Sample size") +
+		  ylab("") +
+          ggtitle(paste0("d = ", d)) +
+          ggsave(
+            filename = paste0("../mmsample/main/img/stats_", sub("\\.", "", d), ".pdf"),
+			width = 7, height = 5
+          )
+  }
